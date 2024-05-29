05127a651fa61cb63bde6d0a3de62d6c867a5853
Layicovaaliya <162481015+Layicovaaliya@users.noreply.github.com>
2024-05-29 16:45:14 +0400

    changeColor/js

---
+++ app.js
@@ -0,0 +1,14 @@
+function changeColor1() {
+    document.getElementById("color").style.backgroundColor = 'white';
+}
+
+// function changeColor2() {
+//     document.getElementById("color").style.backgroundColor = 'white';
+// }
+function changeColor2() {
+    document.getElementById("color").style.backgroundColor = 'black';
+}
+
+// function changeColor4() {
+//     document.getElementById("color").style.backgroundColor = 'white';
+// }
